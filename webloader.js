633304378a9d80c32a20ea4237242b2dfaef8215
Namhyeon, Go <gnh1201@gmail.com>
2020-06-29 00:12:12 +0900

    updated

--- webloader.js
+++ webloader.js
@@ -6,7 +6,7 @@ var FILE = require('lib/file');
 
 return {
 	main: function() {
-        var contents = FILE.readFile("app\\app.html");
+        var contents = FILE.readFile("app\\app.html", "utf-8");
         document.getElementById("app").innerHTML = contents;
 		return 0;
 	}
