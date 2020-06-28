2d34dfc10c7e18b6c4dae88e5f39939d0d25dfc0
Namhyeon, Go <gnh1201@gmail.com>
2020-06-29 01:37:15 +0900

    updated

---
+++ app/assets/js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#btn_login").click(function() {
+        alert("clicked");
+    });
+});
--- webloader.js
+++ webloader.js
@@ -5,9 +5,33 @@
 var FILE = require('lib/file');
 
 return {
+    addScript: function(url, callback) {
+        var el = document.createElement("script");
+        el.src = url;
+        el.type = "text/javascript";
+        el.charset = "utf-8";
+        document.head.appendChild(el);
+        if(typeof(callback) == "function") {
+            el.onload = callback(el);
+        }
+        return el;
+    },
+    addStylesheet: function(s, callback) {
+        var el = document.createElement("link");
+        el.href = url;
+        el.rel = "stylesheet";
+        el.type = "text/css";
+        document.head.appendChild(el);
+        if(typeof(callback) == "function") {
+            el.onload = callback(el);
+        }
+        return el;
+    },
 	main: function() {
         var contents = FILE.readFile("app\\app.html", "utf-8");
         document.getElementById("app").innerHTML = contents;
+        this.addScript("app/assets/js/jquery-3.5.1.min.js");
+        this.addScript("app/assets/js/index.js");
 		return 0;
 	}
 }
