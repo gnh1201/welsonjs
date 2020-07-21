238420c637b0b515a66e908453acd95db10c016b
Namhyeon, Go <gnh1201@gmail.com>
2020-07-21 15:30:18 +0900

    Update bootstrap.js and relative files

--- bootstrap.js
+++ bootstrap.js
@@ -6,19 +6,21 @@
 
 var PS = require("lib/powershell");
 var REG = require("lib/registry");
+var SYS = require("lib/system");
+var SHELL = require("lib/shell");
 
 return {
     main: function() {
-		// unlock file
+        // unlock file
         console.log("Starting unlock files...");
         PS.execCommand("dir | Unblock-File");
 
-		// import necessary registry
-		REG.importFromFile("app/assets/reg/Allow_ADO_CORS.reg");
-		//REG.importFromFile("app/assets/reg/Add_URI_Scheme.reg");
+        // import necessary registry
+        REG.importFromFile("app/assets/reg/Allow_ADO_CORS.reg");
+        //REG.importFromFile("app/assets/reg/Add_URI_Scheme.reg");
 
-		// register URI scheme
-		// todo
+        // register URI scheme
+        //REG.write(hKey, path, key, value, REG.STRING, computer);
 
         console.log("done");
     }
