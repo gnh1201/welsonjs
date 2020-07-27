bff8a2c42a05144dfcb8526f9c0a17aa044d170e
Namhyeon, Go <gnh1201@gmail.com>
2020-07-27 15:41:55 +0900

    Update test-driven components

--- helloworld.js
+++
@@ -1,3 +0,0 @@
-exports.main: function() {
-    console.log("Hello world");
-};
---
+++ test/test.example.js
@@ -0,0 +1,19 @@
+(function() { 
+	return {
+		setUp: function() {
+			inform('Starting...');
+		},
+
+		tearDown: function() {
+			inform('Finishing...')
+		},
+
+		testGreenTest: function () {
+			assert("this is true", true);
+		},
+
+		testRedTest: function () {
+			assert("this is false", false);
+		}
+	};
+})();
