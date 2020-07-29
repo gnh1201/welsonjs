7556c0cce431c3496340e55a0448de05c9771069
Namhyeon, Go <gnh1201@gmail.com>
2020-07-29 15:36:35 +0900

    Update lib/hosts.js, lib/system.js, ssloader.js

--- ssloader.js
+++ ssloader.js
@@ -5,21 +5,28 @@
 var SS = require("lib/shadowsocks");
 var WINTAP = require("lib/wintap");
 var SYS = require("lib/system");
+var HOSTS = require("lib/hosts");
 
 exports.main = function() {
     console.log("Connecting to shadowsocks...");
     var proxyport = SS.connect();
     console.log(proxyport);
-    
-    console.log("Gethering informations of network interfaces");
+
+    console.log("Gethering network interfaces...");
     var inets = SYS.getNetworkInterfaces();
-    for(var i in inets) {
-        console.log(inets[i].Caption);
-        console.log("    > " + inets[i].IPAddresses.join(","));
-        console.log("    > " + inets[i].MACAddress);
+    for (var i in inets) {
+        console.log("Caption > " + inets[i].Caption);
+        console.log("    > IPAddress > " + inets[i].IPAddress);
+        console.log("    > MACAddress > " + inets[i].MACAddress);
     }
 
-    console.log("Installing new WindowsTAP...");
+    console.log("Gethering hosts...");
+    var hosts = HOSTS.getHosts();
+    for (var i = 0; i < hosts.length; i++) {
+        console.log(hosts[i].domain + " -> " + hosts[i].host);
+    }
+
+    console.log("Gethering WindowsTAP interfaces...");
     console.log(WINTAP.query("tap0901"));
     console.log("Done");
 };
