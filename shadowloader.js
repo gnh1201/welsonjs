c38f6a8858d63909f32f527e5b69c875b9312401
고남현 <gnh1201@gmail.com>
2020-10-20 11:41:26 +0900

    enhance feature of CreateObject

--- shadowloader.js
+++ shadowloader.js
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////
-// SSloader
+// ShadowLoader
 ////////////////////////////////////////////////////////////////////////
 
 var SS = require("lib/shadowsocks");
@@ -21,23 +21,24 @@ exports.main = function() {
     console.info("Waiting new launched");
     sleep(10000);
 
-    while(true) {
+    while (true) {
         sleep(10000);
 
         ////////////////////////////////////////////////////////////////
         // LDPlayer
         ////////////////////////////////////////////////////////////////
+
         var LDPList = LDPlayer.getList();
-        for(var i = 0; i < LDPList.length; i++) {
+        for (var i = 0; i < LDPList.length; i++) {
             var pid = parseInt(LDPList[i].PIDVBox);
             var title = LDPList[i].title;
-            if(pid > 0 && PIDList.indexOf(pid) == -1) {
+            if (pid > 0 && PIDList.indexOf(pid) == -1) {
                 console.info("New launched LDPlayer: " + title);
 
                 PIDList.push(pid);
 
                 var listenPort;
-                if(!(title in __config.StaticIP.LDPlayer)) {
+                if (!(title in __config.StaticIP.LDPlayer)) {
                     console.error("Not assigned static IP: " + title);
                     continue;
                 } else {
@@ -53,18 +54,18 @@ exports.main = function() {
                     "-p",
                     pid
                 ]);
-                
+
                 NumSessions = SS.getCountOfSessions();
                 NumBridges = SS.getCountOfBridges();
 
-                if(!(NumSessions > _NumSessions && NumBridges > _NumBridges)) {
-					console.error("Retrying...");
-					PIDList.pop();
-				}
-				
-				_NumSessions = NumSessions;
-				_NumBridges = NumBridges;
-				
+                if (!(NumSessions > _NumSessions && NumBridges > _NumBridges)) {
+                    console.error("Retrying...");
+                    PIDList.pop();
+                }
+
+                _NumSessions = NumSessions;
+                _NumBridges = NumBridges;
+
                 console.info("Waiting new launched");
                 sleep(10000);
             }
@@ -75,16 +76,16 @@ exports.main = function() {
         ////////////////////////////////////////////////////////////////
 
         var NoxPList = NoxPlayer.getList();
-        for(var i = 0; i < NoxPList.length; i++) {
+        for (var i = 0; i < NoxPList.length; i++) {
             var pid = parseInt(NoxPList[i].PID);
             var hostname = NoxPList[i].hostname;
-            if(pid > 0 && PIDList.indexOf(pid) == -1) {
+            if (pid > 0 && PIDList.indexOf(pid) == -1) {
                 console.info("New launched NoxPlayer: " + hostname);
 
                 PIDList.push(pid);
 
                 var listenPort;
-                if(!(hostname in __config.StaticIP.NoxPlayer)) {
+                if (!(hostname in __config.StaticIP.NoxPlayer)) {
                     console.error("Not assigned static IP: " + hostname);
                     continue;
                 } else {
@@ -104,13 +105,13 @@ exports.main = function() {
                 NumSessions = SS.getCountOfSessions();
                 NumBridges = SS.getCountOfBridges();
 
-                if(!(NumSessions > _NumSessions && NumBridges > _NumBridges)) {
-					console.error("Retrying...");
-					PIDList.pop();
-				}
+                if (!(NumSessions > _NumSessions && NumBridges > _NumBridges)) {
+                    console.error("Retrying...");
+                    PIDList.pop();
+                }
 
-				_NumSessions = NumSessions;
-				_NumBridges = NumBridges;
+                _NumSessions = NumSessions;
+                _NumBridges = NumBridges;
 
                 console.info("Waiting new launched");
                 sleep(10000);
@@ -118,4 +119,3 @@ exports.main = function() {
         }
     }
 };
-
