7f3673608a478f06cd81b884cc7eafb8184482e2
고남현 <gnh1201@gmail.com>
2020-10-28 17:18:17 +0900

    fix

--- gtktest.js
+++ gtktest.js
@@ -2,11 +2,64 @@ var GTK = require("lib/gtk");
 
 function main() {
     GTK.init(function() {
-        var win = new GTK.Window();
+        var win, table, button, entry, text, radiogroup, radio1, radio2;
+        
+        // create new window
+        win = new GTK.Window({
+            title: "Hello world",
+            width: 450,
+            height: 400
+        });
+        
+        // create new table
+        table = new GTK.Table({
+            rows: 50,
+            columns: 50
+        });
+        win.setContainer(table);
+
+        // create new button
+        button = new GTK.Button({
+            text: "Exit"
+        });
+        button.addEventListener('click', function() {
+            GTK.exit();
+        });
+        table.attach(button, 41, 49, 45, 49);
+
+        // create new entry
+        entry = new GTK.Entry();
+        table.attach(entry, 1, 40, 45, 49);
+        
+        // create new textbox
+        text = new GTK.TextBox();
+        table.attach(entry, 1, 49, 8, 44);
+        
+        // create new radiogroup
+        radiogroup = new GTK.RadioGroup();
+
+        // create new radio (Radio 1)
+        radio1 = new GTK.RadioBox({
+            text: "Yes",
+            group: radiogroup
+        });
+        table.attach(radio1, 1, 10, 1, 4);
+
+        // create new radio (Radio 2)
+        radio2 = new GTK.RadioBox({
+            text: "No",
+            group: radiogroup
+        });
+        table.attach(radio2, 1, 10, 4, 7);
+
+        // showing window
         win.show();
+        
+        // focusing entry
+        entry.focus();
     });
-    
-    //GTK.wait(function() {});
+
+    GTK.wait();
 }
 
 exports.main = main;
