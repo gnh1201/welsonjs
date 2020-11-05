3bd2e748c74753523d22575d22ce1e67c82ec0b2
고남현 <gnh1201@gmail.com>
2020-11-05 14:32:27 +0900

    fix

--- app/index.js
+++ app/index.js
@@ -7,13 +7,39 @@ var OldBrowser = require("lib/oldbrowser");
 var HTTP = require("lib/http");
 
 var apiUrl = CONFIG.readConfig("/Config/ApiUrl").first().text;
-var token;
+var token, userId;
+
+var showServers = function() {
+    OldBrowser.setContent(FILE.readFile("app\\servers.html", "utf-8"));
+    
+    var req = HTTP.get(apiUrl + "/netsolid/items/assignedservers", "filter[assigned_to][eq]=" + userId, {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "bearer " + token
+    });
+    
+    alert(req.responseText);
+
+    /*
+    var res = JSON.parse(req.responseText);
+
+    for(var i = 0; i < res.data.length; i++) {
+        alert(res.data[i].assigned_to);
+    }
+    */
+};
+
 
 if (FILE.fileExists("token.txt")) {
     token = FILE.readFile("token.txt", "utf-8");
 }
 
-if (typeof(token) === "undefined") {
+if (FILE.fileExists("userid.txt")) {
+    userId = FILE.readFile("userid.txt", "utf-8");
+}
+
+if (typeof(token) !== "undefined") {
+    showServers();
+} else {
     OldBrowser.setContent(FILE.readFile("app\\login.html", "utf-8"));
 
     document.getElementById("loginform").onsubmit = function(ev) {
@@ -37,6 +63,8 @@ if (typeof(token) === "undefined") {
         } else if ("data" in res) {
             console.log("ok");
             FILE.writeFile("token.txt", res.data.token, "utf-8");
+            FILE.writeFile("userid.txt", res.data.user.id, "utf-8");
+            showServers();
         }
     };
 }
