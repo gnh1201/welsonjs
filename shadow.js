20571168c8b0b3a67e320c007d463a09428a2f27
고남현 <gnh1201@gmail.com>
2020-11-10 05:45:13 +0900

    fix

--- shadow.js
+++ shadow.js
@@ -21,9 +21,9 @@ var AppsMutex = [];
 var items = XML.load("staticip.xml").select("/StaticIP/Item").toArray();
 for (var i = 0; i < items.length; i++) {
 	try {
-		var name = items[i].select("Name").first().getText();
-		var uniqueId = items[i].select("UniqueID").first().getText();
-		var ipAddress = items[i].select("IPAddress").first().getText();
+		var name = items[i].getDOM().selectSingleNode("Name").text;
+		var uniqueId = items[i].getDOM().selectSingleNode("UniqueID").text;
+		var ipAddress = items[i].getDOM().selectSingleNode("IPAddress").text;
 
 		if (name in Apps) {
 			Apps[name][uniqueId] = ipAddress;
@@ -78,7 +78,7 @@ var check_NoxPlayer = function() {
 		var hostname = items[i].hostname;
 
 		if (pid > 0 && AppsMutex.indexOf(pid) < 0) {
-			console.info("New launched NoxPlayer: " + title);
+			console.info("New launched NoxPlayer: " + hostname);
 			AppsMutex.push(pid);
 
 			if (hostname in Apps.NoxPlayer) {
@@ -112,7 +112,7 @@ var check_Chrome = function() {
 			console.info("Starting Google Chrome: " + uniqueId);
 
 			listenPort = SS.connect(Apps.Chrome[uniqueId]);
-			Chrome.start("https://naver.com", listenPort, uniqueId);
+			Chrome.start("https://www.showmyip.com/", listenPort, uniqueId);
 			AppsMutex.push("chrome_" + uniqueId);
 		}
 	}
