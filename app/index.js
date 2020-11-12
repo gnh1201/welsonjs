c38ed68ca5539cdb1cdb854b822b0da0c0448760
고남현 <gnh1201@gmail.com>
2020-11-12 12:16:31 +0900

    fix

--- app/index.js
+++ app/index.js
@@ -16,35 +16,111 @@ var apiUrl = CONFIG.readConfig("/ApiUrl").first().getText();
 var servers = [];
 var applications = [];
 
-var getAssignedServers = function() {
-    var assignedServers = [];
+var pingtest = function() {
+    for (var i = 0; i < servers.length; i++) {
+        servers[i].entry.find("span.ping").text(SYS.ping(servers[i].data.ipaddress) + " ms");
+    }
+};
+
+var getLocalApplications = function() {
+    // todo
+};
+
+var getMyApplications = function() {
     var onSuccess = function(res) {
-        return;
-        
+        xmlStrings.push('<?xml version="1.0" encoding="UTF-8"?>');
+        xmlStrings.push("<StaticIP>");
+        for (var i = 0; i < res.data.length; i++) {
+            xmlStrings.push("<Item>");
+            xmlStrings.push("<Name>" + res.data[i].name + "</Name>");
+            xmlStrings.push("<UniqueID>" + res.data[i].unique_id + "</UniqueID>");
+            for (var k = 0; k < servers.length; k++) {
+                if (servers[k].data.id == res.data[i].server) {
+                    xmlStrings.push("<IPAddress>" + servers[k].data.ipaddress + "</IPAddress>");
+                    applications.push({
+                        id: res.data[i].id,
+                        name: res.data[i].name,
+                        uniqueId: res.data[i].unique_id,
+                        server: res.data[i].server,
+                        ipAddress: servers[k].data.ipaddress,
+                        createdBy: userId
+                    });
+                }
+            }
+            xmlStrings.push("</Item>");
+        }
+        xmlStrings.push("</StaticIP>");
+
+        FILE.writeFile("staticip.xml", xmlStrings.join("\r\n"), "utf-8");
+
+        getLocalApplications();
+    };
+
+    HTTP.create()
+        .setContentType("application/x-www-form-urlencoded")
+        .setBearerAuth(token)
+        .setUseCache(false)
+        .get(apiUrl + "/netsolid/items/applications", onSuccess)
+    ;
+};
+
+var getMyServers = function(assignedServers) {
+    var onSuccess = function(res) {
+        var template = $("#listview_servers .template");
+
+        for (var i = 0; i < res.data.length; i++) {
+            if (assignedServers.indexOf(res.data[i].id) > -1) {
+                var entry = template.clone();
+                entry.find("a.title").text(res.data[i].ipaddress);
+                entry.find("div.description").text(res.data[i].name);
+                entry.appendTo("#listview_servers");
+                
+                servers.push({
+                    "data": res.data[i],
+                    "entry": entry
+                });
+            }
+        }
+
+        template.css("display", "none");
+    }
+
+    HTTP.create()
+        .setContentType("application/x-www-form-urlencoded")
+        .setBearerAuth(token)
+        .setUseCache(false)
+        .get(apiUrl + "/netsolid/items/servers", onSuccess)
+    ;
+
+    pingtest();
+    setInterval(pingtest, 5000);
+    document.getElementById("btn_pingtest").onclick = pingtest;
+
+    getMyApplications();
+};
+
+var getAssignedServers = function() {
+    var onSuccess = function(res) {
+        var assignedServers = [];
         for (var i = 0; i < res.data.length; i++) {
             if (res.data[i].assigned_to == userId) {
                 assignedServers.push(res.data[i].server);
             }
         }
+        getMyServers(assignedServers);
     };
 
     HTTP.create()
-        .setHeaders({
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": "bearer " + token,
-            //"Pragma": "no-cache",
-            //"Cache-Control": "no-cache",
-            "If-Modified-Since": "Sat, 1 Jan 2000 00:00:00 GMT"
-        })
-        //.get(apiUrl + "/netsolid/items/assignedservers", onSuccess)
+        .setContentType("application/x-www-form-urlencoded")
+        .setBearerAuth(token)
+        .setUseCache(false)
+        .get(apiUrl + "/netsolid/items/assignedservers", onSuccess)
     ;
 };
 
+/*
 var getMyServers = function() {
-    var assignedServers = getAssignedServers();
-    
-    return;
-    
+    getAssignedServers();
     
     var onSuccess = function(res) {
         var template = $("#listview_servers .template");
@@ -107,7 +183,7 @@ var getMyServers = function() {
         .get(apiUrl + "/netsolid/items/servers", onSuccess)
     ;
 };
-
+*/
 
 if (FILE.fileExists("token.txt")) {
     token = FILE.readFile("token.txt", "utf-8");
@@ -119,7 +195,7 @@ if (FILE.fileExists("userid.txt")) {
 
 if (typeof(token) !== "undefined") {
     OldBrowser.setContent(FILE.readFile("app\\servers.html", "utf-8"));
-    getMyServers();
+    getAssignedServers();
 } else {
     OldBrowser.setContent(FILE.readFile("app\\login.html", "utf-8"));
 
