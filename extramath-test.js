0c211f4055aedb5e08adf2f8b94ea29ac7a5b94e
Namhyeon, Go <gnh1201@gmail.com>
2023-09-07 20:34:01 +0900

    Add files via upload

--- extramath-test.js
+++ extramath-test.js
@@ -2,18 +2,28 @@ var ExtraMath = require("lib/extramath");
 
 function main(args) {
 	var a = "this is an apple";
-	var b = "this is red apple";
-	
-	var dtm = new ExtraMath.DTM();
-	dtm.add(a);
-	dtm.add(b);
-	var mat = dtm.toArray();
+    var b = "this is red apple";
+    
+    var dtm = new ExtraMath.DTM();
+    dtm.add(a);
+    dtm.add(b);
+    var mat = dtm.toArray();
 
-	console.log(mat[0].join(' '));
-	console.log(mat[1].join(' '));
+    console.log("This is a Cosine similarity calculator");
+	console.log("Original sentance");
+    console.log(a);
+    console.log(b);
+	console.log("Done");
 
-	console.log(String(ExtraMath.cos(mat[0], mat[1])));
-	console.log(String(ExtraMath.measureSimilarity(a, b)));
+	console.log("Create a DTM(Document-Term Matrix)");
+    console.log(mat[0].join(' '));
+    console.log(mat[1].join(' '));
+    console.log("Done");
+
+    console.log("Measure Cosine Similarity");
+    console.log('' + ExtraMath.arrayCos(mat[0], mat[1]));
+    console.log('' + ExtraMath.measureSimilarity(a, b));
+    console.log("Done");
 }
 
 exports.main = main;
