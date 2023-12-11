79bc3fe5934e33e3d5414d215519365ae7f987e9
Namhyeon, Go <gnh1201@gmail.com>
2023-12-11 16:54:21 +0900

    Update lib/system.js and related files

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,17 @@
+var SYS = require("lib/system");
+var HTTP = require("lib/http");
+
 function main(args) {
     console.log("Hello world");
     if (typeof WScript !== "undefined") {
-        console.log("Runtime version:", WScript.Version);
+        console.log("Process version:", SYS.getProcessVersion());
+    }
+
+    try {
+        var web = HTTP.create();
+        console.log(web.userAgent);
+    } catch (e) {
+        console.error("Something wrong");
     }
 }
 
