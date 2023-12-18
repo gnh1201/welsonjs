92dedd182ef703e617e85f696353244c2b92f4e7
Namhyeon, Go <gnh1201@gmail.com>
2023-12-19 05:56:16 +0900

    Update officeloader.js

--- officeloader.js
+++ officeloader.js
@@ -6,23 +6,24 @@ var Office = require("lib/msoffice");
 var ChatGPT = require("lib/chatgpt");
 
 function main(args) {
-    // EXAMPLE: cscript app.js officeloader data\example.xlsx
+    // EXAMPLE: cscript app.js officeloader <data\example.xlsx> <programfile>
     if (args.length > 0) {
         var filename = args[0];
-        open(filename);
+        var programfile = args[1];
+        open(filename, programfile);
     } else {
         test();
     }
 }
 
-function open(filename) {
+function open(filename, programfile) {
     var filetypes = [
         {"application": "excel", "filetypes": Office.Excel.SupportedFileTypes},
         {"application": "powerpoint", "filetypes": Office.PowerPoint.SupportedFileTypes},
         {"application": "word", "filetypes": Office.Word.SupportedFileTypes}
     ];
 
-    var resolved_filetype = filetypes.reduce(function(a, x) {
+    var resolved_application = filetypes.reduce(function(a, x) {
         if (a == '') {
             var application = x.application;
             var extensions = x.filetypes.reduce(function(b, x) {
@@ -36,23 +37,37 @@ function open(filename) {
 
         return a;
     }, '');
-    
-    switch (resolved_filetype) {
+
+    var after_opened = function(officeInstance) {
+        if (typeof programfile !== "undefined") {
+            var target = require(programfile);
+            try {
+                target.onApplicationOpened(resolved_application, officeInstance);
+            } catch (e) {
+                console.error("after_opened:", e.message);
+            }
+        };
+    };
+
+    switch (resolved_application) {
         case "excel": {
             var excel = new Office.Excel();   // Create an Excel instance
             excel.open(filename);  // Open the Excel instance
+            after_opened(excel);
             break;
         }
         
         case "powerpoint": {
             var powerpoint = new Office.PowerPoint();   // Create a PowerPoint instance
             powerpoint.open(filename);  // Open the PowerPoint instance
+            after_opened(powerpoint);
             break;
         }
 
         case "word": {
             var word = new Office.Word();   // Create an Word instance
             word.open(filename);  // Open the Word instance
+            after_opened(word);
             break;
         }
 
