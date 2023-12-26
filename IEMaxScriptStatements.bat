3e19d03cd19a34984cb9264868ee7edc18d4f57d
Namhyeon, Go <gnh1201@gmail.com>
2023-12-27 02:10:12 +0900

    IEMaxScriptStatements.bat and registerDLLs.bat is merged to preconfigure.bat

--- IEMaxScriptStatements.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-reg add "HKCU\Software\Microsoft\Internet Explorer\Styles" /f
-reg add "HKCU\Software\Microsoft\Internet Explorer\Styles" /v "MaxScriptStatements" /t REG_DWORD /d 0xFFFFFFFF /f
