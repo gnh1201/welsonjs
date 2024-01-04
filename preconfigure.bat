b1a0e91dd99a0cf1dc1c579c141479eaf20af636
Namhyeon, Go <gnh1201@gmail.com>
2024-01-04 15:55:21 +0900

    Update the native component

--- preconfigure.bat
+++ preconfigure.bat
@@ -15,8 +15,8 @@ regsvr32 /s "%PROGRAMFILES(X86)%\AutoIt3\AutoItX\AutoItX3.dll"
 regsvr32 /s "%PROGRAMFILES(X86)%\AutoIt3\AutoItX\AutoItX3_x64.dll"
 
 echo [*] Registering WelsonJS native component...
-%WINDIR%\Microsoft.NET\Framework\v2.0.50727\RegAsm.exe /codebase WelsonJS.Toolkit.dll
-%WINDIR%\Microsoft.NET\Framework64\v2.0.50727\RegAsm.exe /codebase WelsonJS.Toolkit.dll
+%WINDIR%\Microsoft.NET\Framework\v2.0.50727\RegAsm.exe /codebase bin\x86\WelsonJS.Toolkit.dll
+%WINDIR%\Microsoft.NET\Framework64\v2.0.50727\RegAsm.exe /codebase bin\x64\WelsonJS.Toolkit.dll
 
 echo Done
 
