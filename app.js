ebb254061dbc5bf2c49f7d9f7c7e3c87309cbc9a
Namhyeon, Go <gnh1201@gmail.com>
2024-07-22 00:08:00 +0900

    Update app.js

--- app.js
+++ app.js
@@ -225,21 +225,15 @@ function require(pathname) {
     if ('.js$.jse$.coffee$.ls$.ts$.re$.res$.enc$'.indexOf(suffix + '$') < 0) FN += ".js";
     if (cache[FN]) return cache[FN];
 
-    var T = '';
+    var T = null;
     if (FN.indexOf('://') > -1 && require._scriptProviders.length > 0) {
         // get a script from a custom provider (e.g., remote server)
-        var status = -1, i = 0;
-        while (status < 0 && i < require._scriptProviders.length) {
+        while (T == null && i < require._scriptProviders.length) {
             try {
-                var result = require._scriptProviders[i](FN);
-                status = result.status;
-                if (status > -1) {
-                    T = result.responseText;
-                }
+                T = require._scriptProviders[i](FN) || null;
                 break;
             } catch (e) {
-                status = -1;
-                T = '';
+                T = null;
             }
             i++;
         }
