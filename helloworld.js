8c247adedef0716eecda94fe619ccf63baeadaec
Namhyeon, Go <gnh1201@gmail.com>
2024-07-23 00:37:12 +0900

    Update WelsonJS.Service binaries and example

--- helloworld.js
+++ helloworld.js
@@ -18,4 +18,19 @@ function main(args) {
     //Toolkit.create();
 }
 
+function onServiceStart() {
+    return "onServiceStart recevied";
+}
+
+function onServiceStop() {
+    return "onServiceStop recevied";
+}
+
+function onServiceElapsedTime() {
+    return "onServiceElapsedTime recevied";
+}
+
 exports.main = main;
+exports.onServiceStart = onServiceStart;
+exports.onServiceStop = onServiceStop;
+exports.onServiceElapsedTime = onServiceElapsedTime;
