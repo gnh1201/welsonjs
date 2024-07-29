02d45ede0fd498d22a24e2eb72d8ba7d24941f6e
Namhyeon, Go <gnh1201@gmail.com>
2024-07-29 15:40:11 +0900

    Revert "Update defaultService.js"
    This reverts commit ffd6cde177af26f885c24a5d0c6d2360f803ea52.

--- defaultService.js
+++ defaultService.js
@@ -14,12 +14,7 @@ function onServiceElapsedTime() {
     return "onServiceElapsedTime recevied";
 }
 
-function onServiceScreenTime(filename, screenNumber, x, y, maxCorrelation) {
-    return "onServiceScreenTime recevied (" + [filename, screenNumber, x, y, maxCorrelation].join(',') + ")";
-}
-
 exports.main = main;
 exports.onServiceStart = onServiceStart;
 exports.onServiceStop = onServiceStop;
 exports.onServiceElapsedTime = onServiceElapsedTime;
-exports.onServiceScreenTime = onServiceScreenTime;
