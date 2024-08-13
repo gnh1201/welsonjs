d01d28fbf3bdad783e6d61dff8d30b29ad67b2db
Namhyeon, Go <gnh1201@gmail.com>
2024-08-13 17:05:48 +0900

    Update defaultService.js

--- defaultService.js
+++ defaultService.js
@@ -18,8 +18,18 @@ function onServiceScreenTime(args) {
     return "onServiceScreenTime recevied. " + args.join(', ');
 }
 
+function onFileCreated(args) {
+    return "onFileCreated recevied. " + args.join(', ');
+}
+
+function onFileRuleMatched(args) {
+    return "onFileCreated recevied. " + args.join(', ');
+}
+
 exports.main = main;
 exports.onServiceStart = onServiceStart;
 exports.onServiceStop = onServiceStop;
 exports.onServiceElapsedTime = onServiceElapsedTime;
 exports.onServiceScreenTime = onServiceScreenTime;
+exports.onFileCreated = onFileCreated;
+exports.onFileRuleMatched = onFileRuleMatched;
