ce88ae603b5991997f858734d073f8f85409c645
Namhyeon, Go <gnh1201@gmail.com>
2024-08-22 04:02:31 +0900

    Update WelsonJS.Launcher

--- WelsonJS.Toolkit/WelsonJS.Launcher/Properties/AssemblyInfo.cs
+++ WelsonJS.Toolkit/WelsonJS.Launcher/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
 [assembly: AssemblyTitle("WelsonJS.Launcher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Launcher for WelsonJS Application Package")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WelsonJS.Launcher")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Catswords")]
+[assembly: AssemblyProduct("WelsonJS")]
+[assembly: AssemblyCopyright("Catswords OSS, C-2021-000237, Opensource licensed under GPLv3 or MS-RL")]
+[assembly: AssemblyTrademark("WelsonJS")]
 [assembly: AssemblyCulture("")]
 
 // ComVisible을 false로 설정하면 이 어셈블리의 형식이 COM 구성 요소에 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 번호를
 // 기본값으로 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.7.31")]
+[assembly: AssemblyFileVersion("0.2.7.31")]
