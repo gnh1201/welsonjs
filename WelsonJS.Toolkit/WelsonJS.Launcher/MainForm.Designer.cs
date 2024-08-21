d8558a4398608548f54a3d4ade373c72a416068b
Namhyeon, Go <gnh1201@gmail.com>
2024-08-22 03:36:58 +0900

    Add WelsonJS.Launcher component

---
+++ WelsonJS.Toolkit/WelsonJS.Launcher/MainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WelsonJS.Launcher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = global::WelsonJS.Launcher.Properties.Resources.icon_zip_128;
+            this.button1.Location = new System.Drawing.Point(24, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 200);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "From ZIP file...";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::WelsonJS.Launcher.Properties.Resources.icon_link_128;
+            this.button2.Location = new System.Drawing.Point(230, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 200);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "From external link...";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choose the location of WelsonJS application package.";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(24, 309);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(219, 12);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/gnh1201/welsonjs";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(26, 263);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(184, 16);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "I want to use specific script ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(214, 260);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 345);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "WelsonJS.Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WelsonJS.Toolkit/WelsonJS.Launcher/MainForm.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WelsonJS.Launcher
+{
+    public partial class MainForm : Form
+    {
+        private string workingDirectory;
+        private string appName;
+        private bool waiting = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EnableUI()
+        {
+            label1.Text = "Choose the location of WelsonJS application package.";
+            button1.Enabled = true;
+            button2.Enabled = true;
+            checkBox1.Enabled = true;
+            if (checkBox1.Checked)
+            {
+                textBox1.Enabled = true;
+            }
+        }
+
+        private void DisableUI()
+        {
+            label1.Text = "Please wait...";
+            button1.Enabled = false;
+            button2.Enabled = false;
+            checkBox1.Enabled = false;
+            textBox1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ShowMessageBox("Comming soon...!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = OpenFileDialog();
+            if (filePath != null)
+            {
+                string fileExtension = Path.GetExtension(filePath);
+                if (fileExtension != ".zip")
+                {
+                    ShowMessageBox("It doesn't seems to a ZIP file.");
+                }
+                else
+                {
+                    ExtractAndRun(filePath);
+                }
+            }
+        }
+
+        private void ExtractAndRun(string filePath)
+        {
+            appName = Path.GetFileNameWithoutExtension(filePath);
+            workingDirectory = Path.Combine(Path.GetTempPath(), appName);
+            waiting = true;
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    // If exists, delete all
+                    if (Directory.Exists(workingDirectory))
+                    {
+                        Directory.Delete(workingDirectory, true);
+                    }
+
+                    // try to extact ZIP file
+                    ZipFile.ExtractToDirectory(filePath, workingDirectory);
+
+                    // If it is created the sub-directory
+                    workingDirectory = GetFinalDirectory(workingDirectory);
+
+                    // Run the appliction
+                    RunCommandPrompt();
+					
+					waiting = false;
+                }
+                catch (Exception ex)
+                {
+                    ShowMessageBox(ex.Message);
+                }
+            });
+
+            DisableUI();
+            while (waiting)
+            {
+                Thread.Sleep(1000);
+            }
+            EnableUI();
+        }
+
+        private void RunCommandPrompt()
+        {
+            bool isConsoleApplication = checkBox1.Checked;
+
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo("cmd")
+                {
+                    UseShellExecute = false,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = false,
+                    CreateNoWindow = true,
+                    Arguments = "/k",
+                }
+            };
+            process.Start();
+
+            process.StandardInput.WriteLine("pushd " + workingDirectory);
+            process.StandardInput.WriteLine();
+            if (!isConsoleApplication)
+            {
+                process.StandardInput.WriteLine("bootstrap.bat");
+                process.StandardInput.WriteLine();
+            }
+            else
+            {
+                process.StandardInput.WriteLine("start cmd /c cscript app.js " + textBox1.Text);
+                process.StandardInput.WriteLine();
+            }
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+            process.WaitForExit();
+        }
+
+        private string OpenFileDialog()
+        {
+            string filePath = null;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                }
+            }
+
+            return filePath;
+        }
+
+        private void ShowMessageBox(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        private string GetFinalDirectory(string path)
+        {
+            string[] directories = Directory.GetDirectories(path);
+
+            while (directories.Length == 1)
+            {
+                path = directories[0];
+                directories = Directory.GetDirectories(path);
+            }
+
+            return path;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = checkBox1.Checked;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/gnh1201/welsonjs");
+        }
+    }
+}
