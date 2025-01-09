4eb2d4ae06c1790e3272806584465ab2baee2765
Namhyeon, Go <gnh1201@gmail.com>
2025-01-09 13:58:48 +0900

    Update bootstrap.bat

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,26 +1,44 @@
-REM bootstrap.bat
-REM WelsonJS 0.2.7
-REM https://github.com/gnh1201/welsonjs
+REM bootstrap.bat - WelsonJS 0.2.7
+REM Repository: https://github.com/gnh1201/welsonjs
+
 @echo off
 pushd %~dp0
 
-echo [*] Starting pre-configure script...
+:: Define variables
+set TOOLKIT_URL=https://ics.catswords.net/welsonjs_toolkit.cab
+set TOOLKIT_PATH=%TEMP%\welsonjs_toolkit.cab
+set TOOLKIT_EXTRACT_PATH=%TEMP%
+set REGASM_PATH=%WINDIR%\Microsoft.NET\Framework\v2.0.50727\RegAsm.exe
+set TOOLKIT_DLL=%TEMP%\WelsonJS.Toolkit.dll
 
-echo [*] Registering HTA file association...
+echo [*] Initializing WelsonJS pre-configuration...
+
+:: Register HTA file association
+echo [*] Configuring HTA file association...
 reg import app\assets\reg\Default_HTA.reg
 
-echo [*] Unlocking the performance limit of MSHTML...
+:: Unlock performance limit for MSHTML
+echo [*] Unlocking MSHTML performance limits...
 reg add "HKCU\Software\Microsoft\Internet Explorer\Styles" /f
 reg add "HKCU\Software\Microsoft\Internet Explorer\Styles" /v "MaxScriptStatements" /t REG_DWORD /d 0xFFFFFFFF /f
 
-rem echo [*] Registering AutoItX component...
-rem regsvr32 /s "%PROGRAMFILES(X86)%\AutoIt3\AutoItX\AutoItX3.dll"
-rem regsvr32 /s "%PROGRAMFILES(X86)%\AutoIt3\AutoItX\AutoItX3_x64.dll"
+:: Check if TOOLKIT_DLL exists
+if exist "%TOOLKIT_DLL%" (
+    echo [*] Toolkit already exists. Skipping download and extraction.
+) else (
+    :: Download the latest WelsonJS.Toolkit component
+    echo [*] Downloading the latest WelsonJS.Toolkit component...
+    bitsadmin /transfer toolkit_download /download /priority normal %TOOLKIT_URL% %TOOLKIT_PATH%
+    
+    :: Extract the downloaded CAB file
+    echo [*] Extracting WelsonJS.Toolkit component...
+    expand %TOOLKIT_PATH% %TOOLKIT_EXTRACT_PATH%
+)
 
+:: Register the WelsonJS.Toolkit component
 echo [*] Registering WelsonJS.Toolkit component...
-%WINDIR%\Microsoft.NET\Framework\v2.0.50727\RegAsm.exe /codebase bin\x86\WelsonJS.Toolkit.dll
-REM %WINDIR%\Microsoft.NET\Framework64\v2.0.50727\RegAsm.exe /codebase bin\x64\WelsonJS.Toolkit.dll
+%REGASM_PATH% /codebase %TOOLKIT_DLL%
 
-echo [*] Done.
-
+:: Final step
+echo [*] Pre-configuration complete. Starting bootstrap script...
 cscript app.js bootstrap
