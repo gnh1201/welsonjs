916154499c0347087427813bd4f3746326c831f4
Namhyeon, Go <gnh1201@gmail.com>
2025-04-09 19:58:41 +0900

    Some updates

---
+++ WelsonJS.Toolkit/WelsonJS.Launcher/ResourceTools/AzureAi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace WelsonJS.Launcher.ResourceTools
+{
+    public class AzureAi : IResourceTool
+    {
+        private ResourceServer Server;
+        private const string Prefix = "azure-ai/";
+
+        public AzureAi(ResourceServer server)
+        {
+            Server = server;
+        }
+
+        public bool CanHandle(string path)
+        {
+            return path.StartsWith(Prefix, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public async Task HandleAsync(HttpListenerContext context, string path)
+        {
+            await Task.Delay(0);
+
+            // TODO: Pass Azure AI request to Azure AI Foundry
+        }
+    }
+}
