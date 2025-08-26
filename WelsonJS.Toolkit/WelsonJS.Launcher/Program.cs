57d045405197ece099e048186d6b450eb1a3a0b8
Namhyeon, Go <gnh1201@gmail.com>
2025-08-26 12:49:37 +0900

    Refactor logging and resource server initialization
    Introduces ICompatibleLogger for improved logging, replaces Trace.TraceError and Trace.TraceInformation calls with logger methods, and standardizes ResourceServer instance naming to _resourceServer. Passes logger instances to MainForm and ResourceServer for consistent logging. Also refactors mutex handling and initialization logic for clarity.

--- WelsonJS.Toolkit/WelsonJS.Launcher/Program.cs
+++ WelsonJS.Toolkit/WelsonJS.Launcher/Program.cs
@@ -15,28 +15,35 @@ namespace WelsonJS.Launcher
 {
     internal static class Program
     {
-        static Mutex mutex;
-        public static ResourceServer _ResourceServer;
+        private static readonly ICompatibleLogger _logger;
+
+        public static Mutex _mutex;
+        public static ResourceServer _resourceServer;
+
+        static Program()
+        {
+            _logger = new TraceLogger();
+        }
 
         [STAThread]
         static void Main()
         {
             // create the mutex
-            mutex = new Mutex(true, "WelsonJS.Launcher", out bool isMutexNotExists);
-            if (!isMutexNotExists)
+            _mutex = new Mutex(true, "WelsonJS.Launcher", out bool isAlreadyRunning);
+            if (!isAlreadyRunning)
             {
-                MessageBox.Show("WelsonJS Launcher already running.");
+                _logger.Error("WelsonJS Launcher already running.");
                 return;
             }
 
             // draw the main form
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new MainForm(_logger));
 
             // destory the mutex
-            mutex.ReleaseMutex();
-            mutex.Dispose();
+            _mutex.ReleaseMutex();
+            _mutex.Dispose();
         }
 
         public static void RunCommandPrompt(string workingDirectory, string entryFileName, string scriptName, bool isConsoleApplication = false, bool isInteractiveServiceAapplication = false)
@@ -150,9 +157,9 @@ namespace WelsonJS.Launcher
         {
             lock(typeof(Program))
             {
-                if (_ResourceServer == null)
+                if (_resourceServer == null)
                 {
-                    _ResourceServer = new ResourceServer(GetAppConfig("ResourceServerPrefix"), "editor.html");
+                    _resourceServer = new ResourceServer(GetAppConfig("ResourceServerPrefix"), "editor.html", _logger);
                 }
             }
         }
