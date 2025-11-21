15e6596285c4d160859790de89b3342b995e0833
Namhyeon, Go <gnh1201@gmail.com>
2025-11-21 11:01:10 +0900

    Fix EXE_PATH case in service scripts
    Updated the EXE_PATH variable in both installService.bat and uninstallService.bat to use 'welsonjs' instead of 'WelsonJS' for consistency and to avoid potential path issues.

--- uninstallService.bat
+++ uninstallService.bat
@@ -7,7 +7,7 @@ REM Set the service name
 set SERVICE_NAME=WelsonJS.Service
 
 REM Set the paths
-set EXE_PATH=%APPDATA%\WelsonJS\bin\WelsonJS.Service.exe
+set EXE_PATH=%APPDATA%\welsonjs\bin\WelsonJS.Service.exe
 set INSTALL_UTIL_PATH=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe
 
 REM Uninstall the service
