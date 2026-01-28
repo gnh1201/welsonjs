55c33d1750c6d3a6ebe15e4a0ea7b2639406ee36
Namhyeon, Go <gnh1201@gmail.com>
2026-01-28 17:56:58 +0900

    Fix logger initialization in ResourceServer constructor
    Corrects the logger assignment in ResourceServer to use the provided logger parameter instead of the uninitialized field. Also adds a commented-out log4net configuration line in AssemblyInfo.cs.

--- WelsonJS.Augmented/WelsonJS.Launcher/Properties/AssemblyInfo.cs
+++ WelsonJS.Augmented/WelsonJS.Launcher/Properties/AssemblyInfo.cs
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.2.7.58")]
 [assembly: AssemblyFileVersion("0.2.7.58")]
+//[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
