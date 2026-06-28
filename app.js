b40ab2cbe2ba1db4a371b3f1c667c8eb939c34e9
Namhyeon, Go <gnh1201@catswords.re.kr>
2026-06-28 18:32:42 +0900

    Update copyright header
    Bump copyright year to 2026 and add 'Catswords OSS' to the list of copyright holders in app.js header.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // app.js
-// Copyright 2019-2025, Namhyeon Go <gnh1201@catswords.re.kr> and the WelsonJS contributors.
+// Copyright 2019-2026, Namhyeon Go <gnh1201@catswords.re.kr>, Catswords OSS, WelsonJS contributors.
 // SPDX-License-Identifier: GPL-3.0-or-later
 // https://github.com/gnh1201/welsonjs
 
